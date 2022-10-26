ce2cf76fbe119298d517eb98fc9eb317ea747860
Abul Kawser <uiu.afser@gmail.com>
2022-10-27 04:45:29 +0600

    Registration Added with email and password

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,4 +1,4 @@
-import { getAuth, GithubAuthProvider, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, GithubAuthProvider, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, updateProfile } from 'firebase/auth';
 import React, { createContext, useEffect, useState } from 'react';
 import app from './../firebase/firebase.config';
 
@@ -9,24 +9,30 @@ const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({children}) => {
   const [user, setUser] = useState(null);
 
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //create-user-google
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
   // create-user-github
   const githubLogin = () => {
     return signInWithPopup(auth, githubProvider);
   };
-
-  //create-user-google
-  const googleLogin = () =>{
-    return signInWithPopup(auth,googleProvider);
-  }
-
+ 
+  //update user Profile
+  const updateUserProfile = (profile) => {
+    return updateProfile(auth.currentUser, profile);
+  };
   // current user catching
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-    if(user===null){
-          console.log("user inside state change", currentUser);
-          setUser(currentUser);
-    }
-      
+      if (user === null) {
+        console.log("user inside state change", currentUser);
+        setUser(currentUser);
+      }
     });
     return () => {
       unSubscribe();
@@ -35,6 +41,8 @@ const AuthProvider = ({children}) => {
 
   const authInfo = {
     user,
+    createUser,
+    updateUserProfile,
     googleLogin,
     githubLogin,
   };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { toast } from 'react-hot-toast';
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../Context/AuthProvider";
 
 const Register = () => {
+    const {createUser} = useContext(AuthContext);
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        createUser(email,password)
+        .then(result=>{
+            const user = result.user;
+            console.log(user);
+            toast.success(`Hey ${name}! Thanks for logging`)
+        })
+        .catch(error=>{
+            console.error("Error: ",error);
+        })
+    }
   return (
-    <Form
+    <Form onSubmit={handleSubmit}
       style={{ marginTop: "50px" }}
       className="border border-dark p-5 bg-dark text-white"
     >
