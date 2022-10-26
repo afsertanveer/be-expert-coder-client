8f28f734eac44ec2d5716cbe3b82e63eb2a3ba39
Abul Kawser <uiu.afser@gmail.com>
2022-10-27 01:23:42 +0600

    env file created to protect primarily the firebase data

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from ;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyANQVzXB4fBplaeDo9vNA1kvlRSwHWiqcE",
-  authDomain: "be-expert-coder.firebaseapp.com",
-  projectId: "be-expert-coder",
-  storageBucket: "be-expert-coder.appspot.com",
-  messagingSenderId: "133748658",
-  appId: "1:133748658:web:e5771c4dd6e977eb0f36ab",
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export default app;
