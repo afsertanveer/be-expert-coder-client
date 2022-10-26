968e5dec933876f0cd9292dfe3f2a5422a4212fd
Abul Kawser <uiu.afser@gmail.com>
2022-10-26 20:03:35 +0600

    header favicon website title added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/website-logo/logo.gif';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
+      <Navbar
+        collapseOnSelect
+        className="nav-background"
+        expand="lg"
+        bg=""
+        variant="dark"
+      >
+        <Container>
+          <Link to='/' id="brand-link">
+            <Image src={Logo} className="w-75"></Image>
+          </Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="menu me-auto">
+              <Link>Courses</Link>
+              <Link>Blogs</Link>
+            </Nav>
+            <Nav className='login'>
+              <Link>Login</Link>
+              <Link>Register</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
 };
 
