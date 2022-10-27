386b244b89fcbd61f11ce7251a427c3f37c41c9b
Abul Kawser <uiu.afser@gmail.com>
2022-10-27 20:50:41 +0600

    footer is made fixed bottom

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-      <div className="text-center mt-5 bg-dark">
+      <div className="text-center mt-5 bg-dark fixed-bottom">
         <span className="text-white fw-bolder">
           Copyright Reserved to this website owner Md.Afser
         </span>
